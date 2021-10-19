bbc07ebcdfa81bf1633520a9b4284f5a60255087
Yicheng ZHANG <876610446@qq.com>
2021-10-19 19:28:58 +0800

    upload faster rcnn code

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ from PIL import Image
 import cv2
 import numpy as np
 import json
-import logging
 
-img_dir = "D:\\Courses\\2021 Semester 2\\Research Project\\Flask/faster-rcnn.pytorch/images/"
-pred_dir = "D:\\Courses\\2021 Semester 2\\Research Project\\Flask/faster-rcnn.pytorch/predicts/"
-json_dir = "D:\\Courses\\2021 Semester 2\\Research Project\\Flask/faster-rcnn.pytorch/json_files/"
+img_dir = "faster-rcnn.pytorch/images/"
+pred_dir = "faster-rcnn.pytorch/predicts/"
+json_dir = "faster-rcnn.pytorch/json_files/"
 THRESHOLD = 0.5
  
 
@@ -23,13 +22,17 @@ def predict():
         file_path = os.path.join(img_dir, i)
         os.remove(file_path)
 
+    for i in os.listdir(pred_dir):
+        file_path = os.path.join(pred_dir, i)
+        os.remove(file_path)
+
     img = request.files.get("file")
     imgName = img.filename
     img.save(os.path.join(img_dir, imgName))
-    os.system("cd faster-rcnn.pytorch && python demo.py --net vgg16 --dataset Emotion --checksession 1 --checkepoch 10 --checkpoint 957 --load_dir models")
-    os.system("cd faster-rcnn.pytorch && python demo.py --net vgg16 --dataset Age --checksession 1 --checkepoch 10 --checkpoint 957 --load_dir models")
-    os.system("cd faster-rcnn.pytorch && python demo.py --net vgg16 --dataset Gender --checksession 1 --checkepoch 10 --checkpoint 957 --load_dir models")
-    os.system("cd faster-rcnn.pytorch && python demo.py --net vgg16 --dataset Race --checksession 1 --checkepoch 10 --checkpoint 957 --load_dir models")
+    os.system("cd faster-rcnn.pytorch && python demo.py --net res101 --dataset Emotion --checksession 1 --checkepoch 10 --checkpoint 1915 --load_dir models")
+    os.system("cd faster-rcnn.pytorch && python demo.py --net res101 --dataset Age --checksession 1 --checkepoch 10 --checkpoint 1915 --load_dir models")
+    os.system("cd faster-rcnn.pytorch && python demo.py --net res101 --dataset Gender --checksession 1 --checkepoch 10 --checkpoint 1915 --load_dir models")
+    os.system("cd faster-rcnn.pytorch && python demo.py --net res101 --dataset Race --checksession 1 --checkepoch 10 --checkpoint 1915 --load_dir models")
     obj_list = []
     
     with open(os.path.join(pred_dir, imgName[:imgName.find(".")] + "_Gender.txt"), 'r') as f:
@@ -43,18 +46,18 @@ def predict():
                 continue
             for obj in obj_list:
                 if abs(obj["x1"] - x1) < 10 and abs(obj["y1"] - y1) < 10 and abs(obj["x2"] - x2) < 10 and abs(obj["y2"] - y2) < 10:
-                    if "Gender_score" not in obj or obj["Gender_score"] < score:
+                    if "gender_score" not in obj or obj["gender_score"] < score:
                         overlap = True
-                    if obj["Gender_score"] < score:
+                    if obj["gender_score"] < score:
                         obj["x1"], obj["y1"], obj["x2"], obj["y2"] = x1, y1, x2, y2
-                        obj["Gender_score"] = score
-                        obj["Gender"] = cls
+                        obj["gender_score"] = score
+                        obj["gender"] = cls
             if not overlap:
                 new_obj = {}
-                new_obj["name"] = "person_" + str(len(obj_list) + 1)
+                new_obj["object"] = len(obj_list) + 1
                 new_obj["x1"], new_obj["y1"], new_obj["x2"], new_obj["y2"] = x1, y1, x2, y2
-                new_obj["Gender_score"] = score
-                new_obj["Gender"] = cls
+                new_obj["gender_score"] = score
+                new_obj["gender_class_label"] = cls
                 obj_list.append(new_obj)
     
     with open(os.path.join(pred_dir, imgName[:imgName.find(".")] + "_Age.txt"), 'r') as f:
@@ -65,9 +68,9 @@ def predict():
             cls = line[5]
             for obj in obj_list:
                 if abs(obj["x1"] - x1) < 10 and abs(obj["y1"] - y1) < 10 and abs(obj["x2"] - x2) < 10 and abs(obj["y2"] - y2) < 10:
-                    if "Age_score" not in obj or obj["Age_score"] < score:
-                        obj["Age_score"] = score
-                        obj["Age"] = cls
+                    if "age_score" not in obj or obj["age_score"] < score:
+                        obj["age_score"] = score
+                        obj["age_class_label"] = cls
 
     with open(os.path.join(pred_dir, imgName[:imgName.find(".")] + "_Emotion.txt"), 'r') as f:
         for line in f:
@@ -76,13 +79,11 @@ def predict():
             score = float(line[4])
             cls = line[5]
             overlap = False
-            if score < THRESHOLD:
-                continue
             for obj in obj_list:
                 if abs(obj["x1"] - x1) < 10 and abs(obj["y1"] - y1) < 10 and abs(obj["x2"] - x2) < 10 and abs(obj["y2"] - y2) < 10:
-                    if "Emotion_score" not in obj or obj["Emotion_score"] < score:
-                        obj["Emotion_score"] = score
-                        obj["Emotion"] = cls
+                    if "emo_score" not in obj or obj["emo_score"] < score:
+                        obj["emo_score"] = score
+                        obj["emo_class_label"] = cls
 
     with open(os.path.join(pred_dir, imgName[:imgName.find(".")] + "_Race.txt"), 'r') as f:
         for line in f:
@@ -92,9 +93,9 @@ def predict():
             cls = line[5]
             for obj in obj_list:
                 if abs(obj["x1"] - x1) < 10 and abs(obj["y1"] - y1) < 10 and abs(obj["x2"] - x2) < 10 and abs(obj["y2"] - y2) < 10:
-                    if "Race_score" not in obj or obj["Race_score"] < score:
-                        obj["Race_score"] = score
-                        obj["Race"] = cls
+                    if "ethnicity_score" not in obj or obj["ethnicity_score"] < score:
+                        obj["ethnicity_score"] = score
+                        obj["ethnicity_class_label"] = cls
 
     resultName = imgName[:imgName.find(".")] + "_det.jpg"
     im = Image.open(os.path.join(img_dir, imgName))
@@ -107,10 +108,6 @@ def predict():
     with open(os.path.join(json_dir, imgName[:imgName.find(".")] + ".json"), 'w', encoding="utf-8") as f:
         # obj_json = json.dumps(obj_list)
         json.dump(obj_list, f)
-
-    for i in os.listdir(pred_dir):
-        file_path = os.path.join(pred_dir, i)
-        os.remove(file_path)
         
 
     return send_from_directory(img_dir, os.path.join(img_dir, resultName), resultName)
@@ -125,7 +122,7 @@ def return_json():
         file_path = os.path.join(json_dir, i)
         os.remove(file_path)
 
-    return jsonify({"objects": obj_list})
+    return jsonify({"response": obj_list})
     # return jsonify({"objects":[{"name": "Person_1", "Age": "0-3", "Gender": "Male", "Race": "Asian", "Emotion": "Anger"}, 
     # {"name": "Person_2", "Age": "20-39", "Gender": "Female", "Emotion": "Happiness"}]})
     # return jsonify({"objects": []})
@@ -143,7 +140,7 @@ def drawBoundingBoxes(imageData, inferenceResults):
         y1 = int(res['y1'])
         x2 = int(res['x2']) 
         y2 = int(res['y2']) 
-        label = res['name']
+        label = "Person_" + str(res['object'])
         color = (255, 199, 50)
         imgHeight, imgWidth, _ = imageData.shape
         thick = int((imgHeight + imgWidth) // 300)
@@ -154,4 +151,4 @@ def drawBoundingBoxes(imageData, inferenceResults):
     
 if __name__ == '__main__':
     app.debug = False
-    app.run(host='localhost', port=5000)
+    app.run(host='0.0.0.0', port=5000)
